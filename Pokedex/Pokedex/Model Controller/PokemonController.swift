58c27a906d16ee837362d3a52921d98ba250c4d2
Mitchell Budge <mitchellbudge@gmail.com>
2019-09-06 18:42:51 -0600

    Configure model object and model controller

---
+++ Pokedex/Pokedex/Model Controller/PokemonController.swift
@@ -0,0 +1,13 @@
+//
+//  PokemonController.swift
+//  Pokedex
+//
+//  Created by Mitchell Budge on 9/6/19.
+//  Copyright © 2019 Mitchell Budge. All rights reserved.
+//
+
+import Foundation
+
+class PokemonController {
+    
+}
---
+++ Pokedex/Pokedex/Model/Pokemon.swift
@@ -0,0 +1,13 @@
+//
+//  Pokemon.swift
+//  Pokedex
+//
+//  Created by Mitchell Budge on 9/6/19.
+//  Copyright © 2019 Mitchell Budge. All rights reserved.
+//
+
+import Foundation
+
+struct Pokemon {
+    
+}
