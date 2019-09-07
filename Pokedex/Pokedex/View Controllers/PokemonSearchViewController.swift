ca9a621057068bedd4af20c918a09c87e7f64b97
Mitchell Budge <mitchellbudge@gmail.com>
2019-09-06 18:55:34 -0600

    Refactor detail view controller networking call

--- Pokedex/Pokedex/View Controllers/PokemonSearchViewController.swift
+++ Pokedex/Pokedex/View Controllers/PokemonSearchViewController.swift
@@ -10,10 +10,29 @@ import UIKit
 
 class PokemonSearchViewController: UIViewController, UISearchBarDelegate {
 
+    @IBOutlet weak var searchBar: UISearchBar!
+    var pokemonController = PokemonController()
+    var pokemon: Pokemon?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        pokemonController.searchForPokemon(with: searchBar.text!) { (error) in
+            if let error = error {
+                NSLog("Error fetching Pokemon information: \(error)")
+                return
+            }
+            DispatchQueue.main.async {
+                self.updateViews()
+            }
+        }
+    }
     
+    func updateViews() {
+        guard let pokemon = pokemonController.pokemon else { return }
+        title = pokemon.name
+    }
 
 }
