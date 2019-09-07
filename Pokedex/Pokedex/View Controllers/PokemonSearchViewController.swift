01b27561c5c2b21c9f289d9b143fccae6335434c
Mitchell Budge <mitchellbudge@gmail.com>
2019-09-06 18:40:55 -0600

    Organize files, set up info.plist

---
+++ Pokedex/Pokedex/View Controllers/PokemonSearchViewController.swift
@@ -0,0 +1,19 @@
+//
+//  PokemonSearchViewController.swift
+//  Pokedex
+//
+//  Created by Mitchell Budge on 9/6/19.
+//  Copyright © 2019 Mitchell Budge. All rights reserved.
+//
+
+import UIKit
+
+class PokemonSearchViewController: UIViewController, UISearchBarDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+
+}
